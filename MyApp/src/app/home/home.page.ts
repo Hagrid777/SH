0cbfeda83966f04eaba172e6052508f5fd961810
Hagrid777 <televizripult@gmail.com>
2019-07-02 19:05:10 +0400

    halfwork

--- MyApp/src/app/home/home.page.ts
+++ MyApp/src/app/home/home.page.ts
@@ -6,6 +6,18 @@ declare var Tone: any;
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  sounds = {
+    sound1:new Tone.Player("beats/beat1.mp3").toMaster(),
+    sound2:new Tone.Player("beats/beat2.mp3").toMaster(),
+    sound3:new Tone.Player("beats/beat3.mp3").toMaster(),
+    sound4:new Tone.Player("beats/beat4.mp3").toMaster(),
+    sound5:new Tone.Player("beats/guitare1.mp3").toMaster(),
+    sound6:new Tone.Player("beats/guitare2.mp3").toMaster(),
+    sound7:new Tone.Player("beats/guitare3.mp3").toMaster(),
+    sound8:new Tone.Player("beats/guitare4.mp3").toMaster(),
+  }
+
   tmm = false;
   tmr = false;
   tma = false;
@@ -23,145 +35,161 @@ export class HomePage {
   tmo = false;
   tmy = false;
   change() {
-    this.tmr=!this.tmr
+    this.tmr=!this.tmr;
+    if(this.tmr)Tone.context.resume().then(() => {this.sounds.sound1.start()});
+    this.tmm = false;
+    this.tma = false;
+    this.tmb = false;
   }
-  change1(){
-    this.tmm=!this.tmm
+  change1() {
+    this.tmm = !this.tmm;
+    if(this.tmm)Tone.context.resume().then(() => {this.sounds.sound2.start()});
+    this.tmr = false;
+    this.tma = false;
+    this.tmb = false;
   }
-  change2(){
-    this.tma=!this.tma
+  change2() {
+    this.tma = !this.tma;
+    if(this.tma)Tone.context.resume().then(() => {this.sounds.sound3.start()});
+    this.tmr = false;
+    this.tmm = false;
+    this.tmb = false;
   }
-  change3(){
-    this.tmb=!this.tmb
+  change3() {
+    this.tmb = !this.tmb;
+    if(this.tmb)Tone.context.resume().then(() => {this.sounds.sound4.start()});
+    this.tmr = false;
+    this.tma = false;
+    this.tmm = false;
   }
-  change4(){
-    this.tmc=!this.tmc
+  change4() {
+    this.tmc = !this.tmc
+    if(this.tmc)Tone.context.resume().then(() => {this.sounds.sound5.start()})
+    this.tme = false;
+    this.tmd = false;
+    this.tmf = false;
   }
-  change5(){
-    this.tmd=!this.tmd
+  change5() {
+    this.tmd = !this.tmd
+    if(this.tmd)Tone.context.resume().then(() => {this.sounds.sound6.start()})
+    this.tme = false;
+    this.tmc = false;
+    this.tmf = false;
   }
-  change6(){
-    this.tme=!this.tme
+  change6() {
+    this.tme = !this.tme
+    if(this.tme)Tone.context.resume().then(() => {this.sounds.sound7.start()})
+    this.tmc = false;
+    this.tmd = false;
+    this.tmf = false;
   }
-  change7(){
-    this.tmf=!this.tmf
+  change7() {
+    this.tmf = !this.tmf
+    if(this.tmf)Tone.context.resume().then(() => {this.sounds.sound8.start()})
+    this.tme = false;
+    this.tmd = false;
+    this.tmc = false;
   }
-  change8(){
-    this.tmg=!this.tmg
+  change8() {
+    this.tmg = !this.tmg
   }
-  change9(){
-    this.tmh=!this.tmh
+  change9() {
+    this.tmh = !this.tmh
   }
-  change10(){
-    this.tmi=!this.tmi
+  change10() {
+    this.tmi = !this.tmi
   }
-  change11(){
-    this.tmj=!this.tmj
+  change11() {
+    this.tmj = !this.tmj
   }
-  change12(){
-    this.tmk=!this.tmk
+  change12() {
+    this.tmk = !this.tmk
   }
-  change13(){
-    this.tml=!this.tml
+  change13() {
+    this.tml = !this.tml
   }
-  change14(){
-    this.tmo=!this.tmo
+  change14() {
+    this.tmo = !this.tmo
   }
-  change15(){
-    this.tmy=!this.tmy
+  change15() {
+    this.tmy = !this.tmy
   }
-  deleteAll(){
-    this.tmr= false;
-    this.tmm=false
-    this.tma=false
-    this.tmb=false
-    this.tmc=false
-    this.tmd=false
-    this.tme=false
-    this.tmf=false
-    this.tmg=false
-    this.tmh=false
-    this.tmi=false
-    this.tmj=false
-    this.tmk=false
-    this.tml=false
-    this.tmo=false
-    this.tmy=false
+  deleteAll() {
+    this.tmr = false;
+    this.tmm = false
+    this.tma = false
+    this.tmb = false
+    this.tmc = false
+    this.tmd = false
+    this.tme = false
+    this.tmf = false
+    this.tmg = false
+    this.tmh = false
+    this.tmi = false
+    this.tmj = false
+    this.tmk = false
+    this.tml = false
+    this.tmo = false
+    this.tmy = false
   }
 
-  ngOnInit(){
-  setInterval(() => {
-    var synth = new Tone.Synth().toMaster()
-    if(this.tmr)synth.triggerAttackRelease('A3', '4n');
-  }, 500);
+  ngOnInit() {
+    setInterval(() => {
+      if(this.tmr)Tone.context.resume().then(() => {this.sounds.sound1.start()});
+    }, 16000);
 
-  setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tmm)synth.triggerAttackRelease('B2', '5n');
-}, 500);
+    setInterval(() => {
+      if(!this.tmr)Tone.context.resume().then(()=>{this.sounds.sound1.stop()})
+    }, 500);
+    setInterval(() => {
+      if(this.tmm)Tone.context.resume().then(() => {this.sounds.sound2.start()});
+    }, 16000);
 
-setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tma)synth.triggerAttackRelease('C4', '6n');
-}, 500);
+    setInterval(() => {
+      if(!this.tmm)Tone.context.resume().then(()=>{this.sounds.sound2.stop()})
+    }, 500);
+    setInterval(() => {
+      if(this.tma)Tone.context.resume().then(() => {this.sounds.sound3.start()});
+    }, 16000);
+    setInterval(() => {
+      if(!this.tma)Tone.context.resume().then(() => {this.sounds.sound3.stop()});
+    }, 500);
+    setInterval(() => {
+      if(this.tmb)Tone.context.resume().then(() => {this.sounds.sound4.start()});
+    }, 16000);
+    setInterval(() => {
+      if(!this.tmb)Tone.context.resume().then(() => {this.sounds.sound4.stop()});
+    }, 500);
+    setInterval(() => {
+      if(this.tmc)Tone.context.resume().then(() => {this.sounds.sound5.start()});
+    }, 16000);
+    setInterval(() => {
+      if(!this.tmc)Tone.context.resume().then(() => {this.sounds.sound5.stop()});
+    }, 500);    
+    setInterval(() => {
+      if(this.tmd)Tone.context.resume().then(() => {this.sounds.sound6.start()});
+    }, 16000);
+    setInterval(() => {
+      if(!this.tmd)Tone.context.resume().then(() => {this.sounds.sound6.stop()});
+    }, 500);
+    setInterval(() => {
+      if(this.tme)Tone.context.resume().then(() => {this.sounds.sound7.start()});
+    }, 16000);
+    setInterval(() => {
+      if(!this.tme)Tone.context.resume().then(() => {this.sounds.sound7.stop()});
+    }, 500);
+    setInterval(() => {
+      if(this.tmf)Tone.context.resume().then(() => {this.sounds.sound8.start()});
+    }, 16000);
+    setInterval(() => {
+      if(!this.tmf)Tone.context.resume().then(() => {this.sounds.sound8.stop()});
+    }, 500);
+    // setInterval(() => {
+    //   var synth = new Tone.Player().toMaster()
+    //   if(this.tmg)synth.triggerAttackRelease('B5', '5n');
+    // }, 500);
 
-setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tmb)synth.triggerAttackRelease('D3', '2n');
-}, 500);
-
-setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tmc)synth.triggerAttackRelease('E2', '4n');
-}, 500);
-
-setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tmd)synth.triggerAttackRelease('F4', '8n');
-}, 500);
-
-setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tme)synth.triggerAttackRelease('C1', '5n');
-}, 500);
-
-setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tmf)synth.triggerAttackRelease('B5', '5n');
-}, 500);
-
-setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tmg)synth.triggerAttackRelease('A3', '8n');
-}, 500);
-setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tmh)synth.triggerAttackRelease('B2', '2n');
-}, 500);setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tmi)synth.triggerAttackRelease('A5', '1n');
-}, 500);setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tmj)synth.triggerAttackRelease('C3', '3n');
-}, 500);setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tmk)synth.triggerAttackRelease('D2', '5n');
-}, 500);setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tml)synth.triggerAttackRelease('B3', '2n');
-}, 500);setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tmo)synth.triggerAttackRelease('A1', '3n');
-}, 500);setInterval(() => {
-  var synth = new Tone.Synth().toMaster()
-  if(this.tmy)synth.triggerAttackRelease('A3', '8n');
-}, 500);
-// setInterval(() => {
-//   var synth = new Tone.Synth().toMaster()
-//   if(this.tmg)synth.triggerAttackRelease('B5', '5n');
-// }, 500);
-
-}
+  }
 
 }
 
